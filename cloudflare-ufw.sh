8076b7be79596f91aebdff729467a1e6c067d343
Technekey <prateek.sscet@gmail.com>
2024-01-04 20:47:58 -0800

    Update cloudflare-ufw.sh for better port rules management

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,16 +1,53 @@
 #!/bin/sh
 
-# Allow all traffic from Cloudflare IPs (no ports restriction)
-for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip comment 'Cloudflare IP'; done
 
-ufw reload > /dev/null
+#this script is updated to do the following:
+# If no arguments are passed to the script
+# Then allow all the traffic on all ports from
+# cloudflare IPs
+# if a port or port list is passed then only those
+# ports will be allowed.
 
-# OTHER EXAMPLE RULES
-# Restrict to port 80
-#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80 comment 'Cloudflare IP'; done
+# I am cleaning up the old cloudflare rules based on
+# the comment, so any old IP/subnet will be cleaned
+# before loading this.
 
-# Restrict to port 443
-#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
+#you may want to add this script to cron
 
-# Restrict to ports 80 & 443
-#for cfip in `curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
+reload_ufw() {
+    ufw reload > /dev/null
+}
+
+clear_old_rules() {
+    for i in $(ufw status numbered |awk '/Cloudflare/{x=gensub(/^\[([ ]*[0-9]+)\].*/,"\\1","g");print x}' |tac);do
+        echo "y"|ufw delete $i
+    done
+}
+
+allow_specific_ports_from_cloudflare() {
+    for arg in "$@"; do
+        # Append the argument to the CSV string
+        port_list+=",$arg"
+    done
+
+    # Remove the leading comma
+    port_list="${port_list:1}"
+    echo "port_list: '$port_list'"
+    for cfip in $(curl -sw '\n' https://www.cloudflare.com/ips-v{4,6}); do
+        if [ "$port_list" == '' ];then
+            ufw allow proto tcp from $cfip comment 'Cloudflare IP';
+        else
+            ufw allow proto tcp from $cfip to any port "$port_list" comment 'Cloudflare IP';
+        fi
+     done
+
+}
+
+#clear existing rules that have 'Cloudflare IP' as comments
+clear_old_rules
+
+#add the rules to allow Cloudflare IPs
+allow_specific_ports_from_cloudflare $@
+
+#reload the Firewall
+reload_ufw
